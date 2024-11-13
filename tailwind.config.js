17f6578a2ed7fe06606e7b6ae1654b642b86854c
arttturslv <arttturslv@gmail.com>
2024-11-13 15:21:24 -0300

    build: added black&white colors in tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,12 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'dark': '#111111',
+      'light': '#FFFFFF', 
+    },
     extend: {},
   },
   plugins: [],

8bc9c113f64787814fe53512aafb9f065289fb99
arttturslv <arttturslv@gmail.com>
2024-11-14 09:34:46 -0300

    build: added react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
 import Home from "./pages/Home"
 import BottomNavigation from "./components/BottomNavigation"
 import AlbumView from "./pages/AlbumView"
+import Profile from "./pages/Profile"
+import FavArtists from "./pages/FavArtists"
+import GenresView from "./pages/GenresView"
 
 function App() {
   return (
     <div className=" bg-dark w-full h-[100vh] flex flex-col" >
-      <AlbumView 
-        Title="LET THERE BE ROCK"
-        SubTitle="AC/DC"
-        ImageSource="https://i.scdn.co/image/ab67616d0000b273a948a3a9d736b84fa2e2deab" 
-        OuvintesQNT={166516}></AlbumView>
+      <GenresView></GenresView>
       <BottomNavigation></BottomNavigation>
     </div>
   )

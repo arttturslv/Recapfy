425512dc96fa2371c480d9df455c4aeb19cb5ed2
arttturslv <arttturslv@gmail.com>
2024-11-18 16:08:34 -0300

    style: minor changes

--- src/components/CardElement.jsx
+++ src/components/CardElement.jsx
@@ -1,9 +1,9 @@
 import { Link } from "react-router-dom";
 
-export default function CardElement({CardTitle}) {
+export default function CardElement({CardTitle, BgSource}) {
     return (
-        <Link to={'/album'} className="w-[8rem] h-[5.5rem] min-w-[8rem] min-h-[5rem] relative m-1">
-            <img className="w-full h-[100%] rounded-md" src="https://st4.depositphotos.com/5647624/21667/i/450/depositphotos_216679232-stock-photo-abstract-bright-green-background.jpg"></img>
+        <Link to={'/album'} className="w-[9rem] h-[5.5rem] min-w-[8rem] min-h-[5rem] relative m-1">
+            <img className="w-full h-[100%] rounded-md" src={BgSource}></img>
             <h5 className="font-bold text-[white] absolute top-2 left-2">{CardTitle}</h5>
         </Link>
     )
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,10 +3,10 @@ import CardElement from "../components/CardElement"
 
 export default function Home() {
     return (
-        <div className="px-6 py-4 text-[white] flex flex-col grow">
-          <div className="space-y-3">
+        <div className="px-6 py-4 text-[white] space-y-2 flex flex-col grow">
+          <div className="space-y-1">
             <h1 className="font-bold text-[rgb(255,255,255)] text-xl">Artistas</h1>
-            <div id="artist-scroll-id" className=" w-[90vw] flex flex-row space-x-2 overflow-y-scroll">
+            <div id="artist-scroll-id" className=" w-[92vw] flex flex-row space-x-2 overflow-y-scroll">
               <ArtistElement ArtistName="Michael Jackson"></ArtistElement>
               <ArtistElement ArtistName="Michael Jackson"></ArtistElement>
               <ArtistElement ArtistName="Michael Jackson"></ArtistElement>
@@ -15,20 +15,19 @@ export default function Home() {
               <ArtistElement ArtistName="Ver mais"></ArtistElement>
             </div>
           </div>
-          <div className="space-y-3">
+          <div className="space-y-1">
             <h1 className="font-bold text-xl">Seus gêneros</h1>
             <div className="flex">
-              <CardElement CardTitle="Todos"></CardElement>
-              <CardElement CardTitle="Todos"></CardElement>
+              <CardElement BgSource="https://i.imgur.com/8DwxSys.png" CardTitle="Todos"></CardElement>
+              <CardElement BgSource="https://i.imgur.com/UOy5o3G.png" CardTitle="Todos"></CardElement>
             </div>
           </div>
-          <div className="space-y-3">
+          <div className="space-y-1">
             <h1 className="font-bold text-xl">Seus principais</h1>
             <div className="flex flex-wrap">
-              <CardElement CardTitle="Todos"></CardElement>
-              <CardElement CardTitle="Todos"></CardElement>
-              <CardElement CardTitle="Todos"></CardElement>
-              <CardElement CardTitle="Todos"></CardElement>
+              <CardElement BgSource="https://i.imgur.com/VKF0XlT.png" CardTitle="Todos"></CardElement>
+              <CardElement BgSource="https://i.imgur.com/HTSFrew.png" CardTitle="Todos"></CardElement>
+              <CardElement BgSource="https://i.imgur.com/VZAyCep.png" CardTitle="Todos"></CardElement>
             </div>
           </div>
         </div>

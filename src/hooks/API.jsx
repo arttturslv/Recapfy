0428a562769c02b8b606c6bf4cf14653c50b9903
arttturslv <arttturslv@gmail.com>
2024-12-04 16:09:58 -0300

    feat: added spotify fetch & env variables

--- src/hooks/API.jsx
+++ src/hooks/API.jsx
@@ -1,8 +1,8 @@
-var client_id = '-------------------';
-var redirect_uri = 'http://localhost:5173/';
-var scope = 'user-read-private user-read-email';
+import { getCurrentUsersProfile, getUsersTopItems } from "./UsersMethods";
+var client_id = import.meta.env.VITE_CLIENT_ID;
+var redirect_uri = import.meta.env.VITE_STATUS === "DEV"? 'http://localhost:5173/': "http://recapfy.artttur.com/";
+var scope = 'user-read-private user-read-email user-top-read';
 var state = generateRandomString(16);
-
 localStorage.setItem("stateKey", state);
 
 function generateRandomString(length) {
@@ -26,16 +26,25 @@ export function SpotifyAuth() {
     window.location.href = url;
 }
 
-export function getAccessToken(hash) {
+export async function getAccessToken(hash) {
     if(hash) {
         const params = new URLSearchParams(hash.substring(1)); // Remove o '#'
         const accessToken = params.get('access_token');
 
-        if(accessToken) {
-            console.log("yes")
-            return accessToken;
-        } 
-    }
+        console.log(accessToken)
 
+        if(accessToken) {
+            await getCurrentUsersProfile(accessToken);
+            const artistsLong = await getUsersTopItems("artists", accessToken, 2, 'long_term');
+            const tracksLong = await getUsersTopItems("tracks", accessToken, 2, 'long_term');
+
+            const artistsMedium = await getUsersTopItems("artists", accessToken, 2, 'medium_term');
+            const tracksMedium = await getUsersTopItems("tracks", accessToken, 2, 'medium_term');
+
+            window.location.href = "/home"
+        } else {
+            console.error("Sei não")
+        }
+    }
     return null;
 }

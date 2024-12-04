971c531c8308a2a551a2040a2e5264a99182d151
arttturslv <arttturslv@gmail.com>
2024-12-04 15:48:50 -0300

    feat: extracted localstorage functions

---
+++ src/hooks/LocalStorage.jsx
@@ -0,0 +1,55 @@
+/**
+ * Receives a name, get item from localStorage and returns the parsed object if found, else, returns null.
+ * @param {string} name - Variable name 
+ * @returns Object || null
+ */
+export function getFromLocal(name) {
+    const item = localStorage.getItem(name);
+    if(item!=null && item!="undefined") {
+        return JSON.parse(item);
+    }
+
+    console.warn("Item não foi encontrado.");
+    return null;
+}
+
+/**
+ * Receives a name and a value, then stringify the value and stores in localStorage, returning a boolean response.
+ * @param {string} name - Variable name 
+ * @param {JSON} value - Variable value 
+ * @returns Boolean
+ */
+export function setToLocal(name, value) {
+    const res = localStorage.setItem(name, JSON.stringify(value));
+    return res;
+}
+
+export function getAllItems(ItemsType) {
+
+    let long_Tracks = getItemsByRange(ItemsType, 'long_term');
+    let medium_Tracks = getItemsByRange(ItemsType, 'medium_term');
+
+    let allTracks = [...long_Tracks.items, ...medium_Tracks.items];
+
+    return {
+        items: allTracks,
+        long_total: long_Tracks.total,
+        medium_total: medium_Tracks.total,
+    }
+}
+
+export function getItemsByRange(ItemsType, time_range) {
+    let itemsRanged;
+
+    switch (ItemsType) {
+        case 'tracks':
+            itemsRanged = getFromLocal('USER_TRACKS'+"_"+time_range);
+            break;
+        case 'artists':
+            itemsRanged = getFromLocal('USER_ARTISTS'+"_"+time_range);
+            break;
+        default:
+            throw new Error("ItemsType is not type 'tracks' || 'artists'.")
+    }
+    return itemsRanged;
+}

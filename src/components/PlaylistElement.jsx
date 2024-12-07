1f64f7960cadc69b88d6bd3500242c27d92a4be6
arttturslv <arttturslv@gmail.com>
2024-12-07 18:50:26 -0300

    style: added url to target blank

--- src/components/PlaylistElement.jsx
+++ src/components/PlaylistElement.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 export default function PlaylistElement ({imageSource, title, url, children}) {
     return (
-        <Link target="_blank" to={url} className=' w-36 sm:w-48 hover:bg-grey/20 py-3 px-3 space-y-1 rounded-lg'> 
+        <Link target="_blank" to={url} className='w-32 sm:w-48 hover:bg-grey/20 py-3 px-2 sm:px-3 space-y-1 rounded-lg'> 
             <img 
             className='w-full rounded-md'
             src={imageSource} alt="" srcset="" />
